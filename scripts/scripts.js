cef4b26c7525395329503c41f1b612697d86326a
Jakub <jakub.mika94@gmail.com>
2020-06-21 21:24:02 +0100

    Mobile version by Marcin

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+var hamburger = document.querySelector(".hamburger");
+var nav = document.querySelector(".nav");
+
+hamburger.addEventListener("click", function(e){
+    if (nav.classList.contains("active")) { // 
+        nav.classList.remove("active")
+    } else {
+        nav.classList.add("active")
+    }
+})
