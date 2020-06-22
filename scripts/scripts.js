b24310f6dca53fd923345ec89ce5ccb14c050052
Jakub <jakub.mika94@gmail.com>
2020-06-22 21:00:20 +0100

    change menu botton after use

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,18 @@
 var hamburger = document.querySelector(".hamburger");
 var nav = document.querySelector(".nav");
+var hamburgerIcon = document.querySelector(".hamburger_close")
+var hamburgerMenu = document.querySelector(".hamburger_menu")
 
 hamburger.addEventListener("click", function(e){
     if (nav.classList.contains("active")) {
         nav.classList.remove("active")
+        hamburgerIcon.classList.remove("active")
+        hamburgerMenu.classList.remove("active")
+
     } else {
         nav.classList.add("active")
+        hamburgerIcon.classList.add("active")
+        hamburgerMenu.classList.add("active")
+        
     }
 })
