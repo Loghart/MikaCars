d1a35b5fa6518d8322984156698483301a820687
Jakub <jakub.mika94@gmail.com>
2020-06-22 07:20:53 +0100

    Removes excessive space (nav-menu).  Mobile logo.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,7 @@ var hamburger = document.querySelector(".hamburger");
 var nav = document.querySelector(".nav");
 
 hamburger.addEventListener("click", function(e){
-    if (nav.classList.contains("active")) { // 
+    if (nav.classList.contains("active")) {
         nav.classList.remove("active")
     } else {
         nav.classList.add("active")
